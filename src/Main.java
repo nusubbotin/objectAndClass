744578c91bf286db46e34308b714ce4c04217a26
nusubbotin <nu.subbotin@ya.ru>
2022-10-05 15:33:39 +0300

    Задание 1 к уроку "1.9. Объекты и классы "

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
+        taks1();
+    }
+
+    private static void taks1() {
+        System.out.println("Задание 1:");
+
+        Author perov = new Author("Petr", "Perov");
+        Author sidorov = new Author( "Seny", "Sidorov");
+
+        Book deskBook = new Book("Настольная книга", perov, 2005);
+        System.out.println("deskBook.toString(): " + deskBook.toString());
+
+        Book tutorialBook = new Book("Самоучитель", sidorov, 2001);
+        System.out.println("tutorialBook.toString():" + tutorialBook.toString());
+
+        tutorialBook.setPublicationYear(2015);
+        System.out.println("tutorialBook.toString(): " + tutorialBook.toString());
 
     }
 }
